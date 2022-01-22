e71691ea9ea5fcbd94298d7b6ad52cdd86845311
qwerty287 <ndev@web.de>
2022-01-22 10:03:27 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module codeberg.org/qwerty287/gitea-release-attacher
+
+go 1.17
+
+require code.gitea.io/sdk/gitea v0.15.1
+
+require (
+	code.gitea.io/gitea-vet v0.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/hashicorp/go-version v1.2.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/yuin/goldmark v1.1.25 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/mod v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/tools v0.0.0-20200325010219-a49f79bcc224 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"os"
+	"strings"
+
+	"code.gitea.io/sdk/gitea"
+)
+
+var (
+	instance = flag.String("instance", "", "Gitea instance")
+	token = flag.String("token", "", "Gitea API token")
+	user = flag.String("user", "", "repo owner")
+	repo = flag.String("repo", "", "repo name")
+	path = flag.String("path", "", "filepath to be attached")
+)
+
+func main() {
+	flag.Parse()
+	
+	if *instance == "" || *user == "" || *repo == "" || *path == "" {
+		fmt.Println("incorrect arguments")
+		os.Exit(1)
+	}
+	c, err := gitea.NewClient(*instance, gitea.SetToken(*token))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	releases, _, err := c.ListReleases(*user, *repo, gitea.ListReleasesOptions{
+		ListOptions: gitea.ListOptions{
+			PageSize: 1,
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if len(releases) != 1 {
+		fmt.Println("no releases")
+		os.Exit(1)
+	}
+
+	file, err := os.OpenFile(*path, os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	p := strings.Split(*path, "/")
+	_, _, err = c.CreateReleaseAttachment(*user, *repo, releases[0].ID, file, p[len(p) - 1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
